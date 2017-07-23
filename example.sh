b531c2ee43617357aa42d9413b7cb1472a8f9c3e
yaacov <kobi.zamir@gmail.com>
2017-07-23 10:50:53 +0300

    update setup script

--- example.sh
+++ example.sh
@@ -5,18 +5,15 @@
 # we will use this value to set the route to the metrics service
 export MOHAWK_URL="mohawk.$(ifconfig eth0 | grep 'inet ' | cut -f 10 -d' ').nip.io"
 
+# download the mohawk template file
+wget https://raw.githubusercontent.com/yaacov/mohawk-container/master/mohawk-template.yaml
+
 # delete running mohawk pod, route and service
 oc process -f mohawk-template.yaml | oc delete -f -
 
 # set mohawk pod with nip.io route
 #   in this example we overide the default ROUTE_URL ( default value is mohawk.${MASTER_URL} )
 oc new-app -f mohawk-template.yaml -p ROUTE_URL="${MOHAWK_URL}"
-
-# check if server is running
-# --------------------------
-
-curl -k https://${MOHAWK_URL}/hawkular/metrics/status
-curl -k https://${MOHAWK_URL}/hawkular/metrics/metrics -H "Hawkular-Tenant: _system"
 
 # setting the openshift consule
 # -----------------------------
@@ -27,3 +24,9 @@ sed -i "/assetConfig:/a  \ \ metricsPublicURL: \"https://${MOHAWK_URL}/hawkular/
 
 # restart openshift
 systemctl restart origin-master.service
+
+# check if server is running
+# --------------------------
+
+curl -k https://${MOHAWK_URL}/hawkular/metrics/status
+curl -k https://${MOHAWK_URL}/hawkular/metrics/metrics -H "Hawkular-Tenant: _system"

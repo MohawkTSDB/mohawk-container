263790cc43ecd5e65067ab1936e27a99777d4bf4
yaacov <kobi.zamir@gmail.com>
2017-09-03 21:43:01 +0300

    use ip instead of ifconfig

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 
 # get system IP , must be run on openshift master
 # we will use this value to set the route to the metrics service
-export MOHAWK_URL="mohawk.$(ifconfig eth0 | grep 'inet ' | cut -f 10 -d' ').nip.io"
+#export MOHAWK_URL="mohawk.$(ifconfig eth0 | grep 'inet ' | cut -f 10 -d' ').nip.io"
+export MOHAWK_URL="mohawk.$(ip addr show eth0 | grep 'inet ' | cut -f 6 -d' ' | cut -f 1 -d '/').nip.io"
 
 # download the mohawk template file
 wget https://raw.githubusercontent.com/yaacov/mohawk-container/master/mohawk-template.yaml
